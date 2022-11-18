f3dbfc36b896618c2dbd1b32f91310e41733938d
sanjaijr19 <sanjaikumar@market-intellect.com>
2022-11-18 14:40:06 +0530

    18/11/22

--- chatproject/chatapp/urls.py
+++ chatproject/chatapp/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from knox import views as knox_views
-# from .views import LoginAPI
 from .views import RegisterAPI
 from rest_framework import routers
 from rest_framework.routers import DefaultRouter
@@ -15,13 +13,10 @@ router.register('groupchat',views.GroupChat,basename='groupchat')
 router.register('groupview',views.GroupViewSet,basename='groupview')
 
 urlpatterns = [
-    # path('create/', views.MessageCreate.as_view(), name='create'),
     path('user/<int:pk>', views.UserDetails.as_view(), name='user'),
     path('msg/<int:pk>/', views.MessageDetails.as_view(), name='msg'),
     path('grp/<int:pk>/', views.GroupDetails.as_view(), name='grp'),
     path('grpname/<int:pk>/', views.GroupNameDetails.as_view(), name='grpname'),
-    # path('grpchat/<int:pk>', views.GroupChatDetails.as_view(), name='groupdet'),
-    # path('grpview/', views.GroupView.as_view(), name='GroupView'),
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/',TokenObtainPairView.as_view(),name='login'),
     path('refreshtoken/',TokenRefreshView.as_view(),name='refreshtoken'),
