4398b1e69eba993ffb2abe0ea5770d994fb9cb07
sanjaijr19 <sanjaikumar@market-intellect.com>
2022-11-18 14:47:42 +0530

    18/11/22

--- chatproject/chatapp/models.py
+++ chatproject/chatapp/models.py
@@ -2,22 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-# class GroupMembers(models.Model):
-#     group_members = models.OneToOneField(User,on_delete=models.CASCADE, related_name='group', unique=True)
-#     join_date = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return str(self.group_members)
-#
-#     # def __str__(self):
-#     #     return str(self.join_date)
-#     # def __str__(self):
-#     #     return "%s %s" % (self.group_members, self.join_date)
-#
-#     def Join(self):
-#         self.join_date = timezone.now()
-#         self.save()
-
 
 class GroupName(models.Model):
     name = models.CharField(max_length=20)
