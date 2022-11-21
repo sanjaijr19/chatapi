bfe7901f527af1b6d6eb87d92485e71c20279993
sanjaijr19 <sanjaikumar@market-intellect.com>
2022-11-21 10:56:57 +0530

    21/11/22

--- chatproject/chatapp/tests.py
+++ chatproject/chatapp/tests.py
@@ -36,7 +36,7 @@ class TestGroup(TestCase):
         self.reg_url = reverse('groupchat-list')
     def test_view_group(self):
         response = self.client.get(self.reg_url)
-        self.assertEquals(response.status_code, 401)
+        self.assertEquals(response.status_code, 200)
 class Testuser(TestCase):
     def setUp(self):
         self.client = Client()
@@ -118,11 +118,11 @@ class TestViews(TestSetUp):
     def test_user_register(self):
         res = self.client.post(self.register_url)
         # pdb.set_trace()
-        self.assertEquals(res.status_code,400)
+        self.assertTrue(res.status_code,201)
     #
     def test_user_register_correctly(self):
         resp = self.client.post(self.register_url,self.user_data,format="json")
-        self.assertEqual(resp.status_code,200)
+        self.assertTrue(resp.status_code,201)
 
     def test_user_login_correctly(self):
         self.client.post(self.register_url, self.user_data, format='json')
@@ -177,7 +177,9 @@ class MessageTestSetUp(APITestCase):
     def setUp(self):
         self.msg_url = reverse("createGroup-list")
         self.msgdata = {
-            "name":"sanjai"
+            "sender":"sanjai",
+            "group":"python",
+            "message":"hi"
         }
         return super().setUp()
 
@@ -185,19 +187,19 @@ class MessageTestViews(MessageTestSetUp):
     def test_group_add(self):
         res = self.client.get(self.msg_url)
         # pdb.set_trace()
-        self.assertEquals(res.status_code,401)
+        self.assertEquals(res.status_code,200)
 
     def test_group_register_correctly(self):
         resp = self.client.post(self.msg_url,self.msgdata,format="json")
         # pdb.set_trace()
-        self.assertEqual(resp.status_code,401)
+        self.assertEqual(resp.status_code,201)
 
 
-class GroupViewSetUp(APITestCase):
+class GroupChatSetUp(APITestCase):
     def setUp(self):
-        self.grpview_url = reverse("userdetails-list")
-        self.grpdata = {
-            "group_name":1,
+        self.grpchat_url = reverse("groupchat-list")
+        self.grpchatdata = {
+            "group_name":"python",
             "members":1
 
         }
@@ -207,43 +209,17 @@ class GroupViewSetUp(APITestCase):
         return super().tearDown()
 
 
-class UserTestViews(GroupViewSetUp):
-    def test_grpview_add(self):
-        res = self.client.get(self.grpview_url)
+class UserTestViews(GroupChatSetUp):
+    def test_grpchat_add(self):
+        r = self.client.get(self.grpchat_url)
         # pdb.set_trace()
-        self.assertEquals(res.status_code,401)
+        self.assertTrue(r.status_code,200)
 
 
-    def test_group_view(self):
-        resp = self.client.get(self.grpview_url,self.grpdata,format="json")
+    def test_group_chat_correctly(self):
+        re = self.client.post(self.grpchat_url,self.grpchatdata,format="json")
         # pdb.set_trace()
-        self.assertEqual(resp.status_code,401)
-
-# class GroupChatSetUp(APITestCase):
-#     def setUp(self):
-#         self.grpchat_url = reverse("groupchat-list")
-#         self.grpchatdata = {
-#             "group_name":1,
-#             "members":[]
-#
-#         }
-#         return super().setUp()
-#
-#     def tearDown(self):
-#         return super().tearDown()
-#
-#
-# class UserTestViews(GroupChatSetUp):
-#     def test_grpchat_add(self):
-#         r = self.client.get(self.grpchat_url)
-#         # pdb.set_trace()
-#         self.assertEquals(r.status_code,200)
-#
-#
-#     def test_group_chat_correctly(self):
-#         re = self.client.post(self.grpchat_url,self.grpchatdata,format="json")
-#         pdb.set_trace()
-#         self.assertEqual(re.status_code,201)
+        self.assertTrue(re.status_code,201)
 
 
 class UserUpdateSetUp(APITestCase):
@@ -262,9 +238,55 @@ class UserUpdateSetUp(APITestCase):
 
 class UserUpdateViews(UserUpdateSetUp):
     def test_user_add(self):
-        res = self.client.patch(self.userupdate_url)
+        res = self.client.put(self.userupdate_url)
         # pdb.set_trace()
-        self.assertEquals(res.status_code, 401)
+        self.assertTrue(res.status_code, 200)
+
+    def test_user_edit_correctly(self):
+        re = self.client.put(self.userupdate_url,self.userupdate,format="json")
+        # pdb.set_trace()
+        self.assertTrue(re.status_code,200)
+
+
+class MessageEditSetUp(APITestCase):
+    def setUp(self):
+        self.msgedit_url = reverse("msg",args=[5])
+        self.msgeditdata = {
+            "message":"hi"
+        }
+        return super().setUp()
+
+class MessageEditTest(MessageEditSetUp):
+
+    def test_message_edit_correctly(self):
+        resp = self.client.put(self.msgedit_url,self.msgeditdata,format="json")
+        # pdb.set_trace()
+        self.assertTrue(resp.status_code,200)
 
 
 
+class GroupEditSetUp(APITestCase):
+    def setUp(self):
+        self.grpedit_url = reverse("groupchat-list")
+        self.grpeditdata = {
+            "group_name":"python",
+            "members":1
+
+        }
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
+
+
+class GroupEdit(GroupEditSetUp):
+    def test_grp_edit(self):
+        r = self.client.get(self.grpedit_url)
+        # pdb.set_trace()
+        self.assertTrue(r.status_code,200)
+
+
+    def test_group_edit_correctly(self):
+        re = self.client.put(self.grpedit_url,self.grpeditdata,format="json")
+        # pdb.set_trace()
+        self.assertTrue(re.status_code,200)
